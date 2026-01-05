b048de5581770a33386bf6bb4229398a47894c71
amirreza <amirrezaazizi538@gmail.com>
2026-01-05 21:23:59 +0330

    eslah

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,13 +1,11 @@
 const menubtn = document.getElementById("hamburger");
 const closeMenu = document.getElementById("closeIcon");
 const mainMenu = document.querySelector(".menu-phone-mobile");
-const body = document.querySelector(".body");
 
 const menuOpen = () => {
     menubtn.style.display = "none";
     closeMenu.style.display = "inline-block";
     mainMenu.style.right = "0px";
-    body.style.color = "#cececeff";
 }
 menubtn.addEventListener("click", menuOpen);
 
